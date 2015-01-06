81b99990570bde0deeff3495c816cec4c7e734e3
Rolando Ruiz <rolandoruge@gmail.com>
2015-01-05 20:02:16 -0600

    Se coloca en pocisión el nombre designado como "Rolando"

--- EvergreenJim/src/com/quasar/EvergreenJim/MyGdxGame.java
+++ EvergreenJim/src/com/quasar/EvergreenJim/MyGdxGame.java
@@ -24,6 +24,7 @@ public class MyGdxGame implements ApplicationListener {
 		System.out.println("Roger");
 		System.out.println("Fred");
 		System.out.println("Angel");
+		System.out.println("Rolando");
 		
 		
 		//-----------
