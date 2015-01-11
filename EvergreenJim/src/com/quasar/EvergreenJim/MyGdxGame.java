026dbc15a42aa0989f89da1e0ae7d352e1c8678f
Jose Gzz <josegzza@gmail.com>
2015-01-11 14:57:58 -0600

    La texture aparece correctamente en el eje x.

--- EvergreenJim/src/com/quasar/EvergreenJim/MyGdxGame.java
+++ EvergreenJim/src/com/quasar/EvergreenJim/MyGdxGame.java
@@ -14,6 +14,7 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 import com.quasar.EvergreenJim.Constants;
 
 public class MyGdxGame implements ApplicationListener{
@@ -21,32 +22,27 @@ public class MyGdxGame implements ApplicationListener{
 	private SpriteBatch batch;
 	private Texture texture;
 	private Sprite sprite;
-	//private Monkey m;
-	AssetManager manager = new AssetManager();
+	private MonkeyRegion mr;
+	private Rectangle monkey;
 	
 	@Override
 	public void create() {		
 
-		//new Monkey();
-		//-----------
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
-		
-	//	Gdx.app.log("AssetPath", Gdx.files.internal("assets/square.png").file().getAbsolutePath());
-	//	manager.load("square.png", Texture.class);
-////		
-//		texture = new Texture(Gdx.files.internal("square.png"));
-//		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-//		
-//		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-//		
-//		sprite = new Sprite(region);
-//		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-//		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-//		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		texture = new Texture(Gdx.files.internal("test.png"));
+		camera = new OrthographicCamera();
+	    camera.setToOrtho(false, 800, 480);
+	    batch = new SpriteBatch();
+	    
+	    mr = new MonkeyRegion();
+	    
+//	    
+//	   
+	    monkey = new Rectangle();
+	    monkey.x = (mr.r3.xmax + mr.r3.xmin)/2; // 64?
+	    monkey.y = (mr.r3.ymax + mr.r3.ymin)/2; // bottom left corner of the bucket is 20 pixels above the bottom screen edge
+	    monkey.width = 64;
+	    monkey.height = 64;
+
 	}
 
 	@Override
@@ -60,15 +56,14 @@ public class MyGdxGame implements ApplicationListener{
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		
 		Region mr1 = (Region) new MonkeyRegion().r1;
 		Region mr2 = (Region) new MonkeyRegion().r2;
 		Region mr3 = (Region) new MonkeyRegion().r3;
 		
-//		if(manager.update()) {}
-//		
-//			
-//		System.out.println(manager.getProgress());
-//		
+
 		
 			if(Tap.isInside(mr1)) {
 				System.out.println("Region 1");
@@ -83,13 +78,14 @@ public class MyGdxGame implements ApplicationListener{
 				}
 			}
 			
-			
-			
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+	    batch.draw(texture, monkey.x, monkey.y);
+		batch.end();
 		
-//		batch.setProjectionMatrix(camera.combined);
-//		batch.begin();
-//		//sprite.draw(batch);
-//		batch.end();
+		
+		
+		
 	}
 
 	@Override
