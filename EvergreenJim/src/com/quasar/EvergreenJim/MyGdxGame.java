7aef204e0c99b0c095b407901e53e2598afebb4e
Jose Gzz <josegzza@gmail.com>
2015-01-11 22:28:46 -0600

    Implementa la función de la texture de moverse hacia el click en la primera región del mono.

--- EvergreenJim/src/com/quasar/EvergreenJim/MyGdxGame.java
+++ EvergreenJim/src/com/quasar/EvergreenJim/MyGdxGame.java
@@ -5,17 +5,13 @@ import Regions.Region;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
-import com.quasar.EvergreenJim.Constants;
+import com.badlogic.gdx.math.Vector3;
 
 public class MyGdxGame implements ApplicationListener{
 	private OrthographicCamera camera;
@@ -24,6 +20,7 @@ public class MyGdxGame implements ApplicationListener{
 	private Sprite sprite;
 	private MonkeyRegion mr;
 	private Rectangle monkey;
+	boolean inpos;
 	
 	@Override
 	public void create() {		
@@ -39,7 +36,7 @@ public class MyGdxGame implements ApplicationListener{
 	    monkey.y = (mr.r3.ymax + mr.r3.ymin)/2 - 234; // Corregir esto
 	    monkey.width = 64;
 	    monkey.height = 64;
-
+	    inpos = true;
 	}
 
 	@Override
@@ -60,12 +57,14 @@ public class MyGdxGame implements ApplicationListener{
 		Region mr2 = (Region) new MonkeyRegion().r2;
 		Region mr3 = (Region) new MonkeyRegion().r3;
 		
-
 		
 			if(Tap.isInside(mr1)) {
+				inpos = false;
+		       
 				System.out.println("Region 1");
-				
+					
 			}
+		       
 			else if (Tap.isInside(mr2))
 				System.out.println("Region 2");
 			else if (Tap.isInside(mr3))
@@ -76,10 +75,19 @@ public class MyGdxGame implements ApplicationListener{
 				}
 			}
 			
-			
-			
-			
-			
+
+			 if(!inpos && (monkey.y < Gdx.graphics.getHeight() +50) ) {
+				 monkey.y += 500 * Gdx.graphics.getDeltaTime();
+			 } else
+				 monkey.y += 0 * Gdx.graphics.getDeltaTime();
+			 
+			 if(monkey.y >= Gdx.graphics.getHeight() +50 ) {
+				 System.out.println("got");
+				 inpos = true;
+			 }
+			 
+			 
+			 
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 	    batch.draw(texture, monkey.x, monkey.y);
