d4a29b2f7edb78c5f434ee4bb5411e288786ca04
Roger Luna <Roger  Luna@192.168.0.100>
2015-01-13 22:19:48 -0600

    Corrige valores de velocidad

--- EvergreenJim/src/com/quasar/EvergreenJim/Constants.java
+++ EvergreenJim/src/com/quasar/EvergreenJim/Constants.java
@@ -2,9 +2,9 @@ package com.quasar.EvergreenJim;
 
 public class Constants {
 
-	final static int MAX_SPEED = 10;
+	final static int MAX_SPEED = 1500;
 
 	final static int MID_SPEED = 500;
 
-	final static int MIN_SPEED = 1500;
+	final static int MIN_SPEED = 10;
 }
